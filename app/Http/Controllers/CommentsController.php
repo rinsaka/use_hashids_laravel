360b4290255bafe2e0e289452c2901676df32234
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-06-20 10:05:22 +0900

    hashidsの利用

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Hashids\Hashids;
+// sudo yum install --enablerepo=remi --enablerepo=remi-php71 php-gmp
+
 
 class CommentsController extends Controller
 {
   public function index() {
-    dd('comments@index');
+    $hashids = new Hashids();
+    $hash_id = $hashids->encode(1);
+    dd('comments@index', $hash_id);
   }
 }
